1f9f48c7470d652eadd4a633519021b0ad369e96
Adrian <adunham95@gmail.cooom>
2021-02-08 10:00:06 -0500

    Made custom about page

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
-import Layout from '../components/Layout'
-import Content, { HTMLContent } from '../components/Content'
+import Layout from '../../components/Layout'
+import Content, { HTMLContent } from '../../components/Content'
 
 export const AboutPageTemplate = ({ title, content, contentComponent }) => {
   const PageContent = contentComponent || Content
@@ -14,9 +14,9 @@ export const AboutPageTemplate = ({ title, content, contentComponent }) => {
           <div className="column is-10 is-offset-1">
             <div className="section">
               <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
-                {title}
+                This is my about page
               </h2>
-              <PageContent className="content" content={content} />
+              {/* <PageContent className="content" content={content} /> */}
             </div>
           </div>
         </div>
@@ -31,33 +31,18 @@ AboutPageTemplate.propTypes = {
   contentComponent: PropTypes.func,
 }
 
-const AboutPage = ({ data }) => {
-  const { markdownRemark: post } = data
+const AboutPage = () => {
+//   const { markdownRemark: post } = data
 
   return (
     <Layout>
       <AboutPageTemplate
-        contentComponent={HTMLContent}
-        title={post.frontmatter.title}
-        content={post.html}
+        // contentComponent={HTMLContent}
+        // title={post.frontmatter.title}
+        // content={post.html}
       />
     </Layout>
   )
 }
 
-AboutPage.propTypes = {
-  data: PropTypes.object.isRequired,
-}
-
 export default AboutPage
-
-export const aboutPageQuery = graphql`
-  query AboutPage($id: String!) {
-    markdownRemark(id: { eq: $id }) {
-      html
-      frontmatter {
-        title
-      }
-    }
-  }
-`
