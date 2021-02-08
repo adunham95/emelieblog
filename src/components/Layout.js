f4b6fe27b74ba8da2927cb5bed95fbb748f5d803
Adrian <adunham95@gmail.cooom>
2021-02-08 07:49:02 -0500

    Added scss support

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,12 +2,13 @@ import React from 'react'
 import { Helmet } from 'react-helmet'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
-import './all.sass'
 import useSiteMetadata from './SiteMetadata'
 import { withPrefix } from 'gatsby'
+import "./all.scss"
 
 const TemplateWrapper = ({ children }) => {
   const { title, description } = useSiteMetadata()
+
   return (
     <div>
       <Helmet>
